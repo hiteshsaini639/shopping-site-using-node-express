108beb26acf66f8713d9636ecee94ab20700f139
hitesh saini <hiteshsaini639@gmail.com>
2022-09-18 16:46:25 +0530

    Cart and cartItem relation

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -35,6 +37,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constrains: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   .sync()
@@ -49,6 +55,9 @@ sequelize
   })
   .then((user) => {
     //console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
